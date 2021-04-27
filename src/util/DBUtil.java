6ea11cd91fb9f7cdb7cfb94aa61808d2d513d371
hyunyu <hyunyu@DESKTOP-I3GHKOR>
2021-04-27 12:20:55 +0900

    DBUtil추가

---
+++ src/util/DBUtil.java
@@ -0,0 +1,66 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public class DBUtil {
+
+	//console test용
+//	public static Connection getConnection() {
+//		Connection conn = null;
+//		String driverName = "oracle.jdbc.driver.OracleDriver";
+//		String url ="jdbc:oracle:thin:@192.168.0.165:1521:xe"; //192.168.0.172
+//		String userid = "bob", password = "bob";
+//		
+//		try {
+//			Class.forName(driverName);
+//			conn = DriverManager.getConnection(url,userid,password);
+//		} catch (ClassNotFoundException | SQLException e) {
+//			e.printStackTrace();
+//		}
+//		
+//		return conn;
+//	}
+	
+
+	//ConnectionPool ?��?��
+	public static Connection getConnection() {
+		Connection conn = null;
+		Context initContext;
+		try {
+			initContext = new InitialContext();
+			Context envContext  = (Context)initContext.lookup("java:/comp/env");
+			DataSource ds = (DataSource)envContext.lookup("jdbc/myoracle");
+			conn = ds.getConnection();
+		} catch (NamingException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} 
+		return conn;
+		
+	}
+	//2.자원반납
+	
+	public static void dbClose(ResultSet rs, Statement st, Connection conn) {
+		try {
+			if(rs!=null) rs.close();
+			if(st!=null) st.close();
+			if(conn!=null) conn.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+	
+}
