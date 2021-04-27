94ebb74b317c7bc94c4c0f41cae43669153b2f26
buchonsi <buchonis@naver.com>
2021-04-27 12:54:58 +0900

    syu

--- src/hostController/StudioInsertServlet.java
+++ src/hostController/StudioInsertServlet.java
@@ -1,6 +1,8 @@
 package hostController;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -10,6 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import model.StudioResDAO;
+import model.StudioVO;
 
 /**
  * Servlet implementation class StudioInsertServlet
@@ -28,8 +31,22 @@ public class StudioInsertServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		StudioResDAO dao = new StudioResDAO();
-		
-		
+		StudioVO studio = new StudioVO();
+		String[] facility = request.getParameterValues("have");
+		Map<String, String> facilityChk = new HashMap<String, String>();
+		facilityChk.put("studio_have_mic", "0");
+		facilityChk.put("studio_have_park", "0");
+		facilityChk.put("studio_have_shower", "0");
+		facilityChk.put("studio_have_water", "0");
+		facilityChk.put("studio_have_aircon", "0");
+		facilityChk.put("studio_have_heater", "0");
+		facilityChk.put("studio_have_toilet", "0");
+		for(String s:facility) {
+			if(facilityChk.containsKey(s)) {
+				facilityChk.replace(s, "1");
+			}
+		}
+		for(Map)
 	}
 
 }
