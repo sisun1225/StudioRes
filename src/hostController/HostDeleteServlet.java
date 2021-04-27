2d966ecab999492c1fab2a8591cd0acdf3c53a35
hyunyu <hyunyu@DESKTOP-I3GHKOR>
2021-04-27 15:16:12 +0900

    yjm servlet+

---
+++ src/hostController/HostDeleteServlet.java
@@ -0,0 +1,32 @@
+package hostController;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.StudioResDAO;
+
+/**
+ * Servlet implementation class HostDeleteServlet
+ */
+@WebServlet("/host/hostDelete")
+public class HostDeleteServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String host_id = request.getParameter("host_id");
+		System.out.println(host_id);
+		if(host_id==null||host_id=="") throw new ServletException("아이디를 찾을 수 없습니다");
+		StudioResDAO dao = new StudioResDAO();
+		int result = dao.deleteHost(host_id);
+		System.out.println(result>0?"삭제성공":"삭제실패");
+		RequestDispatcher rd = request.getRequestDispatcher("hostLogout");
+		rd.forward(request, response);
+	}
+}
---
+++ src/hostController/HostLoginServlet.java
@@ -0,0 +1,54 @@
+package hostController;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.HostVO;
+import model.StudioResDAO;
+
+/**
+ * Servlet implementation class HostLoginServlet
+ */
+@WebServlet("/host/hostLogin")
+public class HostLoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		String page = "hostLogin.jsp";
+		if(session.getAttribute("host_id")!=null) {
+			page = "hostMain.jsp";
+		}
+		RequestDispatcher rd = request.getRequestDispatcher(page);
+		rd.forward(request, response);
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		StudioResDAO dao = new StudioResDAO();
+		
+		String host_id = request.getParameter("host_id");
+		String host_pw = request.getParameter("host_pw");
+		HostVO host = dao.loginChk(host_id,host_pw);
+		String page = "hostLogin.jsp";
+		HttpSession session = request.getSession();
+		if(host != null) {
+			session.setAttribute("host_no", host.getHost_no());
+			session.setAttribute("host_id", host.getHost_id());
+			session.setAttribute("host_pw", host.getHost_pw());
+			session.setAttribute("host_name",host.getHost_name());
+			page = "hostMain.jsp";
+		}
+		RequestDispatcher rd = request.getRequestDispatcher(page);
+		rd.forward(request, response);
+	}
+}
